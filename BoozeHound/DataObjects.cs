aa504a47cfb2ebc02c002f61ba237079233dca24
JSherman29 <joshua.sherman29@gmail.com>
2019-03-13 17:38:01 -0400

    Added sql.net package and created basic data classes for each alcohol type.

---
+++ BoozeHound/DataObjects.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace BoozeHound
+{
+    [Table("Beers")]
+    public class Beer
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Brewery { get; set; }
+        public string Style { get; set; }
+        public float ABV { get; set; }
+        public int IBU { get; set; }
+        public string Notes { get; set; }
+        public float Rating { get; set; }
+    }
+
+    [Table("Wines")]
+    public class Wine
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; }
+        [NotNull]
+        public string Name { get; set; }
+        public string Winery { get; set; }
+        public string Type { get; set; }
+        public float ABV { get; set; }
+        public int Vintage { get; set; }
+        public string Notes { get; set; }
+        [NotNull]
+        public float Rating { get; set; }
+    }
+
+    [Table("Spirits")]
+    public class Spirit
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; }
+        [NotNull]
+        public string Name { get; set; }
+        public string Distiller { get; set; }
+        public string Type { get; set; }
+        public float ABV { get; set; }
+        public int Age { get; set; }
+        public string Notes { get; set; }
+        [NotNull]
+        public float Rating { get; set; }
+    }
+}
